08cd2be9ae735769430c3591d4904ab02daeda4b
Guillermo Farías <guillermodanilo08@gmail.com>
2023-12-30 02:23:53 -0300

    add name to user

--- database/seeders/UserSeeder.ts
+++ database/seeders/UserSeeder.ts
@@ -5,11 +5,13 @@ export default class extends BaseSeeder {
   public async run() {
     await User.createMany([
       {
+        name: 'Steve Rogers',
         email: 'srogers@ufacil.com',
         password: 'steve',
         role: 'admin',
       },
       {
+        name: 'Tony Stark',
         email: 'tstark@ufacil.com',
         password: 'tony',
         role: 'user',
