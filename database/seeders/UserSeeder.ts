660bf4057ec62edd957bf276851ccebeb400574d
Guillermo Farías <guillermodanilo08@gmail.com>
2023-12-29 22:52:03 -0300

    add auth, migrations and models

---
+++ database/seeders/UserSeeder.ts
@@ -0,0 +1,19 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        email: 'srogers@ufacil.com',
+        password: 'steve',
+        role: 'admin',
+      },
+      {
+        email: 'tstark@ufacil.com',
+        password: 'tony',
+        role: 'user',
+      },
+    ])
+  }
+}
